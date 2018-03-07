94a49e7594ee312fcee0d7a1308a94936ef3c367
TFIDM\jerry.cheung <jerry.cheung@tfidm.com>
2018-03-07 19:23:56 +0800

    add webpack-hot-middleware for auto reload

--- scripts/build.js
+++ scripts/build.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import webpack from 'webpack';
 import webpackDevMiddleware from 'webpack-dev-middleware';
+import webpackHotMiddleWare from 'webpack-hot-middleware';
 import config from '../webpack.config.babel.js';
 
 const app = express();
@@ -10,5 +11,6 @@ const compiler = webpack(config);
 app.use(webpackDevMiddleware(compiler, {
   publicPath: "/"
 }));
+app.use(webpackHotMiddleWare(compiler));
 
 app.listen(3000);
