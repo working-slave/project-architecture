cfa8d7094b66812b3307f908fad99eac0d09c3e5
TFIDM\jerry.cheung <jerry.cheung@tfidm.com>
2018-03-07 18:57:23 +0800

    setup express server with about page

--- scripts/build.js
+++ scripts/build.js
@@ -1,8 +1,14 @@
 import express from 'express';
 import webpack from 'webpack';
 import webpackDevMiddleware from 'webpack-dev-middleware';
+import config from '../webpack.config.babel.js';
 
 const app = express();
-const config = '../webpack.config.babel.js';
 
-const compiler = webpack(config)
+const compiler = webpack(config);
+
+app.use(webpackDevMiddleware(compiler, {
+  publicPath: "/"
+}));
+
+app.listen(3000);
